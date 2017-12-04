3dac7a42fcf221fdd0eeeae072f976a36628886f
mayang0001 <mayang0001@sina.com>
2017-12-04 21:32:36 +0800

    add mnist reader

--- src/data_reader.h
+++ src/data_reader.h
@@ -1,9 +1,32 @@
+#include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
 #include <fstream>
 
 class MnistReader {
  public:
-  MnistReader();
-  ReadTrain();
-  ReadTest();
+  MnistReader(const std::string& path, int batch_size) 
+      : file_path_(path), batch_size_(batch_size) {
+    std::ifstream file(path);
+    std::string line;
+    while (getline(file, line)) {
+      std::istringstream words(line);
+      float feature;
+      std::vector<float> features;
+      while (words >> feature) {
+        features.push_back(feature); 
+      }
+      data_.push_back(features);
+    }
+  };
+
+  bool NextBatch(std::vector<std::vector<float>>& data) {
+  
+  }
+
+ private:
+  std::vector<std::vector<float>> data_;
+  std::string file_path_;
+  int batch_size_;
 };
